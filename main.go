3f33d3e209ac7ba0f4cb0e24d1ec1d1aea7b5396
Joshua Kellogg <kellogg.20.joshua@gmail.com>
2024-07-18 14:21:19 -0600

    check for git installation on user machine

--- main.go
+++ main.go
@@ -1,19 +1,23 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
+	"io"
+	"os/exec"
+	"strings"
 
 	"github.com/charmbracelet/log"
 	"github.com/jkellogg01/figure/prompt"
 )
 
-type prompter struct {
-	scanner *bufio.Scanner
-}
-
 func main() {
 	log.SetLevel(log.DebugLevel)
+	haveGit, err := checkGitInstalled()
+	if err != nil {
+		log.Fatal("Failed to check git installation", "error", err)
+	} else if !haveGit {
+		log.Fatal("Could not find git installation on this machine")
+	}
 	p := prompt.NewDefaultPrompter()
 	host := p.TextPrompt("Enter your target git host", "github.com")
 	owner := p.TextPrompt("Enter the owner of your target repo", "")
@@ -28,4 +32,24 @@ func main() {
 	path := fmt.Sprintf("%s%s/%s/%s.git", proto, host, owner, repo)
 	confirm := p.BoolPrompt(fmt.Sprintf("Does %s look right?", path), true)
 	log.Info("", "path", path, "confirm", confirm)
+}
+
+func checkGitInstalled() (bool, error) {
+	cmd := exec.Command("git", "--version")
+	pipe, err := cmd.StdoutPipe()
+	if err != nil {
+		return false, err
+	}
+	if err := cmd.Start(); err != nil {
+		return false, err
+	}
+	data, err := io.ReadAll(pipe)
+	if err != nil {
+		return false, err
+	}
+	if err := cmd.Wait(); err != nil {
+		return false, err
+	}
+	_, found := strings.CutPrefix(string(data), "git version ")
+	return found, nil
 }
