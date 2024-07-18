092645fba31677a616c6ac3e49d2123f80430483
Joshua Kellogg <kellogg.20.joshua@gmail.com>
2024-07-18 09:29:50 -0600

    more minimal prompts

--- main.go
+++ main.go
@@ -1,57 +1,86 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 	"strings"
 
-	"github.com/charmbracelet/huh"
 	"github.com/charmbracelet/log"
 )
 
+type prompter struct {
+	scanner *bufio.Scanner
+}
+
 func main() {
 	log.SetLevel(log.DebugLevel)
-	var formData struct {
-		Host       string
-		Owner      string
-		Repository string
+	p := NewDefaultPrompter()
+	host := p.TextPrompt("Enter your target git host", "github.com")
+	owner := p.TextPrompt("Enter the owner of your target repo", "")
+	repo := p.TextPrompt("Enter the name of your target repo", "dotfiles")
+	ssh := p.BoolPrompt("Do you want to us ssh to clone this repo?", false)
+	var proto string
+	if ssh {
+		proto = "git@"
+	} else {
+		proto = "https://"
 	}
-	f := huh.NewForm(huh.NewGroup(
-		huh.NewInput().
-			Title("Target git host:").
-			Placeholder("github.com").
-			Suggestions([]string{
-				"github.com",
-				"codeberg.org",
-				"gitlab.com",
-			}).
-			Value(&formData.Host),
-		huh.NewInput().
-			Title("Code owner:").
-			Placeholder("probably you?").
-			Value(&formData.Owner),
-		huh.NewInput().
-			Title("Repository name:").
-			Placeholder(".dotfiles").
-			Value(&formData.Repository),
-	))
-	if err := f.Run(); err != nil {
-		log.Fatal(err)
+	path := fmt.Sprintf("%s%s/%s/%s.git", proto, host, owner, repo)
+	confirm := p.BoolPrompt(fmt.Sprintf("Does %s look right?", path), true)
+	log.Info("", "path", path, "confirm", confirm)
+}
+
+func (p *prompter) TextPrompt(q, d string) string {
+	var defaultText string
+	if d != "" {
+		defaultText = fmt.Sprintf("default: %s", d)
 	}
-	path := fmt.Sprintf(
-		"https://%s.git",
-		strings.Join([]string{
-			formData.Host,
-			formData.Owner,
-			formData.Repository},
-			"/"),
-	)
-	var looksGood bool
-	huh.NewConfirm().
-		Title("Your dotfiles should be here, right?").
-		Description(path).
-		Affirmative("That's right!").
-		Negative("No...").
-		Value(&looksGood).
-		Run()
-	log.Info(path, "looks good", looksGood)
+	resp := p.prompt(q, defaultText)
+	if resp == "" {
+		return d
+	}
+	return resp
+}
+
+func (p *prompter) BoolPrompt(q string, d bool) bool {
+	var defaultText string
+	if d {
+		defaultText = "Y/n"
+	} else {
+		defaultText = "y/N"
+	}
+	resp := p.prompt(q, defaultText)
+	if len(resp) == 0 {
+		return d
+	}
+	switch strings.ToLower(resp)[0] {
+	case 'y':
+		return true
+	case 'n':
+		return false
+	default:
+		return d
+	}
+}
+
+func (p *prompter) prompt(q, d string) string {
+	fmt.Print(q)
+	if d != "" {
+		fmt.Printf(" (%s)", d)
+	}
+	fmt.Print("\n> ")
+	if !p.scanner.Scan() {
+		if err := p.scanner.Err(); err != nil {
+			log.Error(err)
+		}
+		return ""
+	}
+	return p.scanner.Text()
+}
+
+func NewDefaultPrompter() *prompter {
+	return &prompter{
+		bufio.NewScanner(os.Stdin),
+	}
 }
