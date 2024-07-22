60dc0ce7f673fd541b837038982ee3bcb008c003
Joshua Kellogg <kellogg.20.joshua@gmail.com>
2024-07-22 15:03:37 -0600

    add confirmation for clean command

--- cmd/clean.go
+++ cmd/clean.go
@@ -3,6 +3,8 @@ package cmd
 import (
 	"os"
 
+	"github.com/charmbracelet/huh"
+	"github.com/charmbracelet/log"
 	"github.com/jkellogg01/figure/files"
 	"github.com/spf13/cobra"
 )
@@ -13,6 +15,20 @@ var cleanCmd = &cobra.Command{
 	Long: `[WARNING] THIS IS A DEVELOPER TOOL! IT IS NOT RECOMMENDED THAT YOU _EVER_ USE THIS COMMAND IN PRODUCTION [WARNING]
 	clean will delete figure's root directory and everything inside it. This is intended to make repeated teasting of the program more convenient and is not recommended for figure users under any circumstances.`,
 	RunE: func(cmd *cobra.Command, args []string) error {
+		var confirm bool
+		err := huh.NewConfirm().
+			Title("WARNING: you are using a DESTRUCTIVE developer tool.").
+			Description("This is not recommended for users under any circumstances.\nAre you sure you want to delete your figure directory and ALL of its contents?").
+			Affirmative("I want to delete all of my data").
+			Negative("That seems bad").
+			Value(&confirm).
+			Run()
+		if err != nil {
+			return err
+		}
+		if !confirm {
+			log.Info("clean cancelled.")
+		}
 		root, err := files.GetFigurePath()
 		if err != nil {
 			return err
