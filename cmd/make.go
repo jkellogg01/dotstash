6bde4ad045b9e6cc5289ffdcd6b4128fcb7692a1
Joshua Kellogg <kellogg.20.joshua@gmail.com>
2024-07-21 20:28:45 -0600

    makes a dotfiles directory for you
    note to self: I was working on some fzf integration to select files to add to your dotfiles directory,
    scrapped it because it was too much wrestling.
    
    could be interesting to come back to though.

--- cmd/make.go
+++ cmd/make.go
@@ -1,42 +1,56 @@
 package cmd
 
 import (
+	"errors"
+	"fmt"
+	"io/fs"
 	"os"
 
-	"github.com/charmbracelet/bubbles/filepicker"
-	"github.com/charmbracelet/bubbles/key"
-	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/log"
 	"github.com/jkellogg01/figure/files"
 	"github.com/spf13/cobra"
 )
 
 var makeCmd = &cobra.Command{
-	Use:   "make [dir]",
+	Use:   "make name [file]...",
 	Short: "set up a git repository and add config files to it",
 	Long: `make starts an interactive command prompt to set up a new git repository for your configuration files.
 	you can optionally specify a starting dir; the prompt will start in $XDG_CONFIG_HOME by default.`,
 	RunE: makeFn,
-	Args: cobra.MaximumNArgs(1),
+	Args: cobra.MinimumNArgs(1),
 }
 
 func makeFn(cmd *cobra.Command, args []string) error {
-	path, err := os.UserConfigDir()
+	figRoot, err := files.GetFigurePath()
 	if err != nil {
 		return err
 	}
-	if len(args) == 1 {
-		argPath := args[0]
-		log.Debugf("attempting to start command prompt in %s", argPath)
-		v, err := files.DirExists(argPath)
+	newCfgPath := figRoot + string(os.PathSeparator) + args[0]
+	err = os.Mkdir(newCfgPath, os.ModeDir|777)
+	if errors.Is(err, fs.ErrExist) {
+		log.Infof("directory '%s' already exists. backing up and replacing...", newCfgPath)
+		i := 0
+		backupPath := fmt.Sprintf("%s_backup_%04d", newCfgPath, i)
+		err = os.Rename(newCfgPath, backupPath)
+		for errors.Is(err, fs.ErrExist) {
+			i++
+			backupPath = fmt.Sprintf("%s_backup_%04d", newCfgPath, i)
+			err = os.Rename(newCfgPath, backupPath)
+		}
 		if err != nil {
+			log.Error("failed to create backup", "error", err)
 			return err
 		}
-		if v {
-			path = argPath
+		err = os.Mkdir(newCfgPath, os.ModeDir|777)
+		if err != nil {
+			log.Error("failed to create new config dir", "error", err)
+			return err
 		}
-		log.Debug("", "path", path)
+	} else if err != nil {
+		log.Error("failed to create new config dir", "error", err)
+		return err
 	}
+	log.Debugf("successfully created a new config dir at %s", newCfgPath)
 	return nil
 }
 
