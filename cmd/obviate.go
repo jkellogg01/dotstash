1708dabb2c62e2bbb760ad494e97c0818c11c775
Joshua Kellogg <kellogg.20.joshua@gmail.com>
2024-07-25 20:51:57 -0600

    most of the logic for obviate
    putting in a commit before i write the method to remove a target from
    the manifest

--- cmd/obviate.go
+++ cmd/obviate.go
@@ -1,19 +1,60 @@
 package cmd
 
 import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"slices"
+
+	"github.com/charmbracelet/log"
+	"github.com/jkellogg01/dotstash/files"
+	"github.com/jkellogg01/dotstash/manifest"
 	"github.com/spf13/cobra"
 )
 
 var obviateCmd = &cobra.Command{
 	Use:     "obviate",
 	Aliases: []string{"rm-config"},
-	Run:     obviateFn,
+	RunE:    obviateFn,
+	Args:    cobra.MinimumNArgs(1),
 }
 
-func obviateFn(cmd *cobra.Command, args []string) {
-
+func obviateFn(cmd *cobra.Command, args []string) error {
+	repos, err := os.ReadDir(dotstashPath)
+	if err != nil {
+		return err
+	}
+	var target os.DirEntry
+	for _, e := range repos {
+		if e.Name() == repoName {
+			target = e
+			break
+		}
+	}
+	if target == nil {
+		return fmt.Errorf("%s is not in your current list of repositories!", repoName)
+	}
+	targetPath := filepath.Join(dotstashPath, target.Name())
+	metadata, err := manifest.ReadManifest(targetPath)
+	if err != nil {
+		return err
+	}
+	for _, t := range metadata.ExpandTargets() {
+		if !slices.Contains(args, filepath.Base(t.Src)) {
+			continue
+		}
+		err := files.Substitute(t.Src, t.Dst)
+		if err != nil {
+			log.Error("substitution failed", "target", t, "error", err)
+		}
+		// TODO: manifest.RemoveTarget
+	}
+	return nil
 }
 
 func init() {
 	rootCmd.AddCommand(obviateCmd)
+
+	obviateCmd.Flags().StringVarP(&repoName, "repository", "r", "", "the repository to remove the config from")
+	obviateCmd.MarkFlagRequired("repository")
 }
