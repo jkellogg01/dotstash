0d618c216da95d8a724774076ae70299e4ae7947
Joshua Kellogg <kellogg.20.joshua@gmail.com>
2024-07-29 21:16:25 -0600

    I think that's it, actually

--- cmd/select.go
+++ cmd/select.go
@@ -4,9 +4,13 @@ import (
 	"fmt"
 	"path/filepath"
 
+	"github.com/charmbracelet/log"
 	"github.com/jkellogg01/dotstash/manifest"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
+
+var clobber bool
 
 var selectCmd = &cobra.Command{
 	Use:   "select <repo>",
@@ -16,15 +20,27 @@ var selectCmd = &cobra.Command{
 }
 
 func selectFn(cmd *cobra.Command, args []string) error {
+	primary := viper.GetString("primary_config")
+	if primary == args[0] {
+		log.Infof("%s is already your primary configuration!", primary)
+		return nil
+	}
 	path := filepath.Join(dotstashPath, args[0])
-	_, err := manifest.ReadManifest(path)
+	meta, err := manifest.ReadManifest(path)
 	if err != nil {
 		return fmt.Errorf("error reading manifest at %s: %w", path, err)
 	}
-	// TODO: link from metadata targets
+	meta.Link(clobber)
+	viper.Set("primary_config", args[0])
+	err = viper.WriteConfig()
+	if err != nil {
+		log.Errorf("Failed to write config: %v", err)
+	}
 	return nil
 }
 
 func init() {
 	rootCmd.AddCommand(selectCmd)
+
+	selectCmd.Flags().BoolVarP(&clobber, "clobber", "c", false, "delete potentially non-symlink files when replacing them with configuration data from this repository")
 }
