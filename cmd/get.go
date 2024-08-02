0e2764abd97f8e1df0582befd94a8c74e67b4f1c
Joshua Kellogg <kellogg.20.joshua@gmail.com>
2024-08-02 10:47:57 -0600

    skeleton get command

--- cmd/get.go
+++ cmd/get.go
@@ -1,32 +1,31 @@
 package cmd
 
 import (
-	"github.com/charmbracelet/log"
+	"strings"
+
 	"github.com/spf13/cobra"
 )
 
-var (
-	interactive bool
-	atUrl       string
-)
+var branch string
 
 var getCmd = &cobra.Command{
-	Use:   "get [url]",
-	Short: "fetch a remote git repository to apply its configuration",
-	RunE: func(cmd *cobra.Command, args []string) error {
-		log.Debug("get called", "args", args)
-		return nil
-	},
-	Args: func(cmd *cobra.Command, args []string) error {
-		if interactive {
-			return nil
-		}
-		return cobra.ExactArgs(1)(cmd, args)
-	},
+	Use:   "get",
+	Short: "A brief description of your command",
+	RunE:  getFn,
+	Args:  cobra.ExactArgs(1),
+}
+
+func getFn(cmd *cobra.Command, args []string) error {
+	var info strings.Builder
+	info.WriteString(args[0])
+	if branch != "" {
+		info.WriteString(" on branch " + branch)
+	}
+	return nil
 }
 
 func init() {
 	rootCmd.AddCommand(getCmd)
 
-	getCmd.Flags().BoolVarP(&interactive, "interactive", "i", false, "use an interactive prompt for downloading a config")
+	getCmd.Flags().StringVarP(&branch, "branch", "b", "", "specify a branch to download from")
 }
