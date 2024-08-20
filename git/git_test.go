5dbff27ad833c73680fbd61b5cf18bdddd6dbff2
Joshua Kellogg <kellogg.20.joshua@gmail.com>
2024-08-20 17:59:37 -0600

    I am starting to think that wrapping exec.Cmd was a bad idea.

--- git/git_test.go
+++ git/git_test.go
@@ -1,7 +1,6 @@
 package git
 
 import (
-	"os"
 	"os/exec"
 	"strings"
 	"testing"
@@ -14,20 +13,43 @@ func TestInitRepo(t *testing.T) {
 		t.Fatalf("repository initiation failed: %v", err)
 	}
 
-	err = os.Chdir(dir)
-	if err != nil {
-		t.Fatal(err)
-	}
-
 	c := exec.Command(
 		"git",
 		"status",
 	)
+	c.Dir = dir
 	output, err := c.CombinedOutput()
+	if err != nil {
+		t.Error(err)
+	}
+	if strings.Contains("fatal: not a git repository (or any of the parent directories)", string(output)) {
+		t.Error("target repository does not contain a git repository")
+	}
+}
+
+func TestExecInteractive(t *testing.T) {
+	// NOTE: `go test` should just cache the results of TestInitRepo so I don't
+	// feel too bad about the way I'm using it here.
+	if !t.Run("repo initialization works", TestInitRepo) {
+		t.Fatal("this test cannot be run unless TestInitRepo is passing")
+	}
+
+	dir := t.TempDir()
+	err := InitRepo(dir)
+	// NOTE: in this context there should literally never be an error
+	// but handling your errors never hurt anybody
 	if err != nil {
 		t.Fatal(err)
 	}
-	if strings.Contains("fatal: not a git repository (or any of the parent directories)", string(output)) {
-		t.Fatal("target repository does not contain a git repository")
-	}
+
+	go func(t *testing.T) {
+		err = Exec(dir, "commit")
+		if err != nil {
+			// I think this should be fatal but testing doesn't like that
+			t.Error(err)
+		}
+	}(t)
+
+	// TODO: script the input so that we can actually close the editor
+	// TODO: fail if the editor doesn't close when the script is done
 }
