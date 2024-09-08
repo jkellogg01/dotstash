d64b80f5c7120b96c25dda53f727975a2888db61
Joshua Kellogg <kellogg.20.joshua@gmail.com>
2024-09-08 13:14:38 -0600

    more exhaustive testing for manifest compression & expansion

--- manifest/manifest_test.go
+++ manifest/manifest_test.go
@@ -1,6 +1,7 @@
 package manifest
 
 import (
+	"path/filepath"
 	"testing"
 )
 
@@ -11,8 +12,11 @@ func TestCompress(t *testing.T) {
 		expect string
 	}{
 		{"home directory compression", homePath, homePathAbbr},
+		{"home subdirectory compression", filepath.Join(homePath, "testcase"), filepath.Join(homePathAbbr, "testcase")},
 		{"config directory compression", configPath, configPathAbbr},
+		{"config subdirectory compression", filepath.Join(configPath, "testcase"), filepath.Join(configPathAbbr, "testcase")},
 		{"dotstash directory compression", dotstashPath, dotstashPathAbbr},
+		{"dotstash subdirectory compression", filepath.Join(dotstashPath, "testcase"), filepath.Join(dotstashPathAbbr, "testcase")},
 	}
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
@@ -20,6 +24,7 @@ func TestCompress(t *testing.T) {
 			if result != tc.expect {
 				t.Errorf("Expected %s, got %s", tc.expect, result)
 			}
+			t.Logf("compression successful: %s => %s, expect %s", tc.data, result, tc.expect)
 		})
 	}
 }
@@ -31,8 +36,11 @@ func TestExpand(t *testing.T) {
 		expect string
 	}{
 		{"home directory expansion", homePathAbbr, homePath},
+		{"home subdirectory expansion", filepath.Join(homePathAbbr, "testcase"), filepath.Join(homePath, "testcase")},
 		{"config directory expansion", configPathAbbr, configPath},
+		{"config subdirectory expansion", filepath.Join(configPathAbbr, "testcase"), filepath.Join(configPath, "testcase")},
 		{"dotstash directory expansion", dotstashPathAbbr, dotstashPath},
+		{"dotstash subdirectory expansion", filepath.Join(dotstashPathAbbr, "testcase"), filepath.Join(dotstashPath, "testcase")},
 	}
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
@@ -40,6 +48,7 @@ func TestExpand(t *testing.T) {
 			if result != tc.expect {
 				t.Errorf("Expected %s, got %s", tc.expect, result)
 			}
+			t.Logf("expansion successful: %s => %s, expect %s", tc.data, result, tc.expect)
 		})
 	}
 }
