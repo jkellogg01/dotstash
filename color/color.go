379ac954677c533d2acdd25c78053d25d8e50780
Joshua Kellogg <kellogg.20.joshua@gmail.com>
2024-09-09 21:14:52 -0600

    add constants for ANSI colors

---
+++ color/color.go
@@ -0,0 +1,22 @@
+package color
+
+import "github.com/charmbracelet/lipgloss"
+
+const (
+	Black lipgloss.ANSIColor = iota
+	Red
+	Green
+	Yellow
+	Blue
+	Purple
+	Cyan
+	White
+	BrightBlack
+	BrightRed
+	BrightGreen
+	BrightYellow
+	BrightBlue
+	BrightPurple
+	BrightCyan
+	BrightWhite
+)
